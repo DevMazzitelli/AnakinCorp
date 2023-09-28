40e64f6a8a76f7d9caa9890533e617d5266e3315
DevMazzitelli <DevMazzitelli@gmail.com>
2023-09-28 06:45:23 +0200

    fix responsive

--- assets/js/barreNavMobile.js
+++ assets/js/barreNavMobile.js
@@ -4,12 +4,21 @@ document.addEventListener('DOMContentLoaded', function () {
     const subMenuMobile = document.querySelector('.sub-menuMobile');
 
     // Ajoutez un gestionnaire d'événement de clic au bouton
-    nosEquipesMobileBtn.addEventListener('click', function () {
+    nosEquipesMobileBtn.addEventListener('click', function (event) {
+        event.stopPropagation(); // Empêche la propagation du clic au document
         // Inversez la visibilité du sous-menu en ajoutant ou en supprimant la classe 'visible'
         if (subMenuMobile.classList.contains('visible')) {
             subMenuMobile.classList.remove('visible'); // Cacher le sous-menu
         } else {
             subMenuMobile.classList.add('visible'); // Afficher le sous-menu
+        }
+    });
+
+    // Ajoutez un gestionnaire d'événement de clic au document entier
+    document.addEventListener('click', function (event) {
+        // Si l'utilisateur clique en dehors du bouton et du sous-menu, cachez le sous-menu
+        if (event.target !== nosEquipesMobileBtn && !subMenuMobile.contains(event.target)) {
+            subMenuMobile.classList.remove('visible'); // Cacher le sous-menu
         }
     });
 });
